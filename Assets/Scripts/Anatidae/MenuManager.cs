7189ad5b69b8e83cfb33241e4d6dfb6e4d054bf2
Jules Sabater <jules.sab132@outlook.fr>
2024-09-10 12:40:05 +0200

    fix

--- Assets/Scripts/Anatidae/MenuManager.cs
+++ Assets/Scripts/Anatidae/MenuManager.cs
@@ -5,9 +5,9 @@ using TMPro;
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] TMP_Text quitText;
-    const float AfkTime = 20f;
+    const float AfkTime = 200f;
     float afkTimer = 0f;
-    const float HeldQuitTime = 1f;
+    const float HeldQuitTime = 1.5f;
     float heldQuitTimer = 0f;
     const string MenuMessage = "Retour au menu";
 
@@ -27,16 +27,15 @@ public class MenuManager : MonoBehaviour
         else
             heldQuitTimer = 0f;
 
-        // if (Input.GetAxisRaw("P1_Horizontal") == 0 || Input.GetAxisRaw("P1_Vertical") == 0 || Input.GetButton("P1_Start") || Input.GetButton("P1_B1") || Input.GetButton("P1_B2") || Input.GetButton("P1_B3") || Input.GetButton("P1_B4") || Input.GetButton("P1_B5") || Input.GetButton("P1_B6") ||
-        //    Input.GetAxisRaw("P2_Horizontal") == 0 || Input.GetAxisRaw("P2_Vertical") == 0 || Input.GetButton("P2_Start") || Input.GetButton("P2_B1") || Input.GetButton("P2_B2") || Input.GetButton("P2_B3") || Input.GetButton("P2_B4") || Input.GetButton("P2_B5") || Input.GetButton("P2_B6"))
-        if (false)
+        if (Input.GetAxisRaw("P1_Horizontal") != 0 || Input.GetAxisRaw("P1_Vertical") != 0 || Input.GetButton("P1_Start") || Input.GetButton("P1_B1") || Input.GetButton("P1_B2") || Input.GetButton("P1_B3") || Input.GetButton("P1_B4") || Input.GetButton("P1_B5") || Input.GetButton("P1_B6") ||
+            Input.GetAxisRaw("P2_Horizontal") != 0 || Input.GetAxisRaw("P2_Vertical") != 0 || Input.GetButton("P2_Start") || Input.GetButton("P2_B1") || Input.GetButton("P2_B2") || Input.GetButton("P2_B3") || Input.GetButton("P2_B4") || Input.GetButton("P2_B5") || Input.GetButton("P2_B6"))
             afkTimer = 0f;
         else
             afkTimer += Time.deltaTime;
 
         if (heldQuitTimer != 0 || afkTimer - AfkTime + 10f > 0f) {
             quitText.gameObject.SetActive(true);
-            quitText.text = MenuMessage + new string('.', (int)Mathf.Max(heldQuitTimer * 3, afkTimer - AfkTime + 10f * 0.5f));
+            quitText.text = MenuMessage + new string('.', (int)Mathf.Max(heldQuitTimer * 3f, afkTimer - AfkTime + 10f * 0.5f));
         } else quitText.gameObject.SetActive(false);
     }
 
